b4fe32f007ec26df1cd2ef4d23e2ad20de2fb03c
yhh1056 <yhh1056@naver.com>
2021-02-05 20:40:53 +0900

    쿼리 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO member(id, username, password, age, address) VALUES (1, 'yhh', '1234', 27, 'seoul');
+INSERT INTO member(id, username, password, age, address) VALUES (2, 'osh', 'osh123', 43, 'vietnam');
+INSERT INTO member(id, username, password, age, address) VALUES (3, 'aaa', '123', 21, 'seoul');
+INSERT INTO member(id, username, password, age, address) VALUES (4, 'bbb', '12346', 20, 'busan');
+INSERT INTO member(id, username, password, age, address) VALUES (5, 'ccc', 'ccc123', 64, 'incheon');
+INSERT INTO member(id, username, password, age, address) VALUES (6, 'ddd', 'd123', 43, 'seoul');
+INSERT INTO member(id, username, password, age, address) VALUES (7, 'fff', 'f123', 67, 'busan');
+INSERT INTO member(id, username, password, age, address) VALUES (8, 'rrr', 'g123', 45, 'usa');
+INSERT INTO member(id, username, password, age, address) VALUES (9, 'eee', 'r1421', 34, 'changdong');
+INSERT INTO member(id, username, password, age, address) VALUES (10, 'qqq', '2542', 23, 'seoul');
+
